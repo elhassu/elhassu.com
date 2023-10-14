b66abed5508d94ecaa79b8e708d41542f69d9379
Keelan Vella <keelan.x3@gmail.com>
2023-10-14 11:57:59 +0200

    Implemented Hotjar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
+import Hotjar from "@hotjar/browser";
+
+const siteId = 3694121;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
