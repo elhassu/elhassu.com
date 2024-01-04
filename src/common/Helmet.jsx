8a37d74da326432bd65073c7c718f976142b447a
Keelan Vella <keelan.x3@gmail.com>
2024-01-04 02:02:42 +0100

    Add meta tags for Open Graph and Twitter cards

--- src/common/Helmet.jsx
+++ src/common/Helmet.jsx
@@ -1,18 +1,19 @@
 import { Helmet as ReactHelmet } from "react-helmet";
 
-export function Helmet({ title, description, path, keywords }) {
+export function Helmet({
+	title = "Keelan Vella",
+	description = `Keelan Vella, an Entrepreneur and a Full Stack Developer with a relentless passion for software development.`,
+	path,
+	keywords,
+}) {
 	const pagePath = typeof path === "string" ? (path[0] === "/" ? path : `/${path}`) : "/";
 
 	return (
 		<ReactHelmet>
-			<title>{title ? title : `Keelan Vella`}</title>
+			<title>{title}</title>
 			<meta
 				name="description"
-				content={
-					description
-						? description
-						: `Keelan Vella, an Entrepreneur and a Full Stack Developer with a relentless passion for software development.`
-				}
+				content={description}
 			/>
 			<link
 				rel="canonical"
@@ -24,6 +25,34 @@ export function Helmet({ title, description, path, keywords }) {
 					content={keywords}
 				/>
 			)}
+			<meta
+				property="og:url"
+				content={`https://www.elhassu.com${pagePath}`}
+			/>
+			<meta
+				property="og:title"
+				content={title}
+			/>
+			<meta
+				property="og:description"
+				content={description}
+			/>
+			<meta
+				name="twitter:card"
+				content="summary_large_image"
+			/>
+			<meta
+				name="twitter:site"
+				content="@Keelan_El_Hassu"
+			/>
+			<meta
+				name="twitter:title"
+				content={title}
+			/>
+			<meta
+				name="twitter:description"
+				content={description}
+			/>
 		</ReactHelmet>
 	);
 }
